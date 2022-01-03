832a6f043ab3c071a95b4ea36385428f49f737c5
shweta.mbytes <shwetapatil@mirafra.com>
2022-01-03 17:08:11 +0530

    added slave memory map

--- src/hvl_top/slave/axi4_slave_agent_config.sv
+++ src/hvl_top/slave/axi4_slave_agent_config.sv
@@ -20,6 +20,7 @@ class axi4_slave_agent_config extends uvm_object;
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "axi4_slave_agent_config");
+  extern virtual task slave_memory_task(bit [ADDRESS_WIDTH-1:0]slave_address, bit [DATA_WIDTH-1:0]data);
 endclass : axi4_slave_agent_config
 
 //--------------------------------------------------------------------------------------------
@@ -31,6 +32,18 @@ endclass : axi4_slave_agent_config
 function axi4_slave_agent_config::new(string name = "axi4_slave_agent_config");
   super.new(name);
 endfunction : new
+
+//-------------------------------------------------------
+//Task : slave_memory_task
+//Used to store the slave data into the slave memory
+//Parameter :
+//slave_address - bit [ADDRESS_WIDTH-1 :0]
+//data          - bit [DATA_WIDTH-1:0]
+//-------------------------------------------------------
+task axi4_slave_agent_config::slave_memory_task(bit [ADDRESS_WIDTH-1 :0]slave_address, bit [DATA_WIDTH-1:0]data);
+  slave_memory[slave_address] = data;
+endtask : slave_memory_task
+
 
 `endif
 
--- src/hvl_top/test/axi4_base_test.sv
+++ src/hvl_top/test/axi4_base_test.sv
@@ -114,11 +114,21 @@ function void axi4_base_test::setup_axi4_slave_agent_cfg();
   foreach(axi4_env_cfg_h.axi4_slave_agent_cfg_h[i])begin
     axi4_env_cfg_h.axi4_slave_agent_cfg_h[i] =
     axi4_slave_agent_config::type_id::create("axi4_slave_agent_cfg_h");
+    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].slave_id = i;
+    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].slave_selected = 0;
+    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].min_address = axi4_env_cfg_h.axi4_master_agent_cfg_h.master_min_addr_range_array[i];
+    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].max_address = axi4_env_cfg_h.axi4_master_agent_cfg_h.master_max_addr_range_array[i];
+    if(SLAVE_AGENT_ACTIVE ===1) begin
     axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].is_active    = uvm_active_passive_enum'(UVM_ACTIVE);
+    end
+    else begin
+    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].is_active    = uvm_active_passive_enum'(UVM_PASSIVE);
+    end 
     axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].has_coverage = 1; 
-    uvm_config_db
-    #(axi4_slave_agent_config)::set(this,"*axi4_slave_agent*","axi4_slave_agent_config",
-    axi4_env_cfg_h.axi4_slave_agent_cfg_h[i]);
+    uvm_config_db#(axi4_slave_agent_config)::set(this,"*axi4_slave_agent*","axi4_slave_agent_config",
+                   axi4_env_cfg_h.axi4_slave_agent_cfg_h[i]);
+    `uvm_info(get_type_name(),$sformatf("\nAPB_SLAVE_CONFIG[%0d]\n%s",
+              i,axi4_env_cfg_h.axi4_slave_agent_cfg_h[i].sprint()),UVM_LOW);
   end
 endfunction: setup_axi4_slave_agent_cfg
 
