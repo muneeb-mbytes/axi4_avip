921744e264971894e4bab78096dafc967e9efae2
venkateshthota <venkateshthota@mirafra.com>
2022-01-03 17:07:00 +0530

    changed generate block for master_agent_bfm and slave_agent_bfm in hdl_top

--- src/hdl_top/hdl_top.sv
+++ src/hdl_top/hdl_top.sv
@@ -57,48 +57,6 @@ module hdl_top;
               .aresetn(rst));
 
   //-------------------------------------------------------
-  // axi4 master BFM Agent Instantiation
-  //-------------------------------------------------------
-//  genvar i;
-//  generate
-//    for (i=0; i < NO_OF_MASTERS ; i++) begin : apb_master_agent_bfm
-//      axi4_master_agent_bfm #(.MASTER_ID(i)) axi4_mster_agent_bfm_h(intf);
-//      //defparam axi4_master_agent_bfm[i].axi4_master_agent_bfm_h.MASTER_ID = i;
-//    end
-//  endgenerate
-//  
-//  //-------------------------------------------------------
-//  // axi4 slave BFM Agent Instantiation
-//  //-------------------------------------------------------
-//  genvar j;
-//  generate
-//    for (j=0; j < NO_OF_SLAVES; j++) begin : apb_slave_agent_bfm
-//      axi4_slave_agent_bfm #(.SLAVE_ID(j)) axi4_slave_agent_bfm_h(intf);
-//      //defparam axi4_slave_agent_bfm[j].axi4_slave_agent_bfm_h.SLAVE_ID = j;
-//    end
-//  endgenerate
-  
-
-//<<<<<<< HEAD
-  // Variable : master_agent_bfm_h
-  //axi4 Master BFM Agent Instantiation 
-  //axi4_master_agent_bfm master_agent_bfm_h(intf); 
-
-  // Variable : slave_agent_bfm_h
-  // axi4 Slave BFM Agent Instantiation
-  //axi4_slave_agent_bfm slave_agent_bfm_h(intf);
-
-//=======
-//  // Variable : master_agent_bfm_h
-//  //axi4 Master BFM Agent Instantiation 
-//  axi4_master_agent_bfm master_agent_bfm_h(intf); 
-//
-//  // Variable : slave_agent_bfm_h
-//  // axi4 Slave BFM Agent Instantiation
-//  axi4_slave_agent_bfm slave_agent_bfm_h(intf);
-//>>>>>>> 3f5e56cecb0a64d037433ebd7887cfda85dd2647
-
-  //-------------------------------------------------------
   // AXI4  No of Master and Slaves Agent Instantiation
   //-------------------------------------------------------
   genvar i;
