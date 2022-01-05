0961f835a866435a2d24618dd40886eb4427fbac
shweta.mbytes <shwetapatil@mirafra.com>
2022-01-05 16:35:13 +0530

    Modified data and strboe as queues in globals and master converter

--- src/globals/axi4_globals_pkg.sv
+++ src/globals/axi4_globals_pkg.sv
@@ -303,8 +303,8 @@ package axi4_globals_pkg;
     bit	                    awready;
 
     //Write_data_channel
-    bit [DATA_WIDTH-1:0]     wdata;
-    bit [(DATA_WIDTH/8)-1:0] wstrb;
+    bit [DATA_WIDTH-1:0]     wdata[$:DATA_WIDTH];
+    bit [(DATA_WIDTH/8)-1:0] wstrb[$:DATA_WIDTH];
     //bit                      wlast;
 
     //Write Response Channel
@@ -333,7 +333,7 @@ package axi4_globals_pkg;
     bit         arlock;
     //Read Data Channel
     bit [15:0]                rid;
-    bit [DATA_WIDTH-1: 0]     rdata;
+    bit [DATA_WIDTH-1: 0]     rdata[$:DATA_WIDTH];
     bit [(DATA_WIDTH/8)-1: 0] rstrb;
     bit [1:0]                 rresp; 
   } axi4_r_transfer_char_s;
