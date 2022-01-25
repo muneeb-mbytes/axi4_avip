af1f761a09d441bcf5a02c52857cb5e3bc3d3276
Nadeem-Ahmed <nadeemahmed@mirafra.com>
2022-01-25 17:26:02 +0530

    Added the semaphore concept for both write and read in master

--- src/hdl_top/master_agent_bfm/axi4_master_driver_bfm.sv
+++ src/hdl_top/master_agent_bfm/axi4_master_driver_bfm.sv
@@ -236,7 +236,7 @@ interface axi4_master_driver_bfm(input bit aclk,
 
     while(bvalid !== 1'b1) begin
       @(posedge aclk);
-      `uvm_info(name,$sformatf("WAITING FOR WLAST :: %0d",wlast),UVM_HIGH);
+      `uvm_info(name,$sformatf("WAITING FOR BAVLID :: %0d",bvalid),UVM_HIGH);
     end
 
     repeat(data_write_packet.no_of_wait_states)begin
