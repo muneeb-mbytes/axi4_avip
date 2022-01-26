ef7c1d9f9cd89298139cf83fb7fc113c7e72bc1a
Nadeem-Ahmed <nadeemahmed@mirafra.com>
2022-01-26 17:59:41 +0530

    Added the do while loops for all channels in master driver

--- src/hvl_top/master/axi4_master_seq_item_converter.sv
+++ src/hvl_top/master/axi4_master_seq_item_converter.sv
@@ -101,6 +101,8 @@ function void axi4_master_seq_item_converter::from_write_class( input axi4_maste
   `uvm_info("axi4_master_seq_item_conv_class",$sformatf("after writnig wuser =  %0h",output_conv_h.wuser),UVM_HIGH);
 
   output_conv_h.no_of_wait_states = input_conv_h.no_of_wait_states;
+  `uvm_info("axi4_master_seq_item_conv_class",$sformatf("after writnig no_wait_states =  %0d",output_conv_h.no_of_wait_states),UVM_HIGH);
+
   output_conv_h.wait_count_write_address_channel =input_conv_h.wait_count_write_address_channel ;
   output_conv_h.wait_count_write_data_channel =input_conv_h.wait_count_write_data_channel ;
   output_conv_h.wait_count_write_response_channel =input_conv_h.wait_count_write_response_channel ;
@@ -169,6 +171,9 @@ function void axi4_master_seq_item_converter::from_read_class( input axi4_master
 
   output_conv_h.ruser = input_conv_h.ruser;
   `uvm_info("axi4_master_seq_item_conv_class",$sformatf("after writnig ruser =  %0b",output_conv_h.ruser),UVM_HIGH);
+
+  output_conv_h.no_of_wait_states = input_conv_h.no_of_wait_states;
+  `uvm_info("axi4_master_seq_item_conv_class",$sformatf("after writnig no_wait_states =  %0d",output_conv_h.no_of_wait_states),UVM_HIGH);
 
   output_conv_h.wait_count_read_address_channel =input_conv_h.wait_count_read_address_channel ;
   output_conv_h.wait_count_read_data_channel =input_conv_h.wait_count_read_data_channel ;
