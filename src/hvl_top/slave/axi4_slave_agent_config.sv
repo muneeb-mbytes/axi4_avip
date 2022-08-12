16628c9815c130d627f97e113535d2bfda4d44f4
venkateshthota <venkateshthota@mirafra.com>
2022-08-12 22:33:46 +0530

    added prints for OOOR and min and max txns to enable OOOR

--- src/hvl_top/slave/axi4_slave_agent_config.sv
+++ src/hvl_top/slave/axi4_slave_agent_config.sv
@@ -105,6 +105,9 @@ function void axi4_slave_agent_config::do_print(uvm_printer printer);
   printer.print_field ("has_coverage", has_coverage, $bits(has_coverage), UVM_DEC);
   printer.print_field ("min_address",  min_address,  $bits(max_address),  UVM_HEX);
   printer.print_field ("max_address",  max_address,  $bits(max_address),  UVM_HEX);
+  printer.print_field ("out_of_oreder",  out_of_oreder,  $bits(out_of_oreder),  UVM_HEX);
+  printer.print_field ("minimum_transactions",  minimum_transactions,  $bits(minimum_transactions),  UVM_HEX);
+  printer.print_field ("maximum_transactions",  maximum_transactions,  $bits(maximum_transactions),  UVM_HEX);
   printer.print_field ("wait_count_write_response_channel",wait_count_write_response_channel,$bits(wait_count_write_response_channel),UVM_DEC);
   printer.print_field ("wait_count_read_data_channel",wait_count_read_data_channel,$bits(wait_count_read_data_channel),UVM_DEC);
          
